b6f93cb5a0e54d9b457616baa23c98877c7458c6
marc23321 <jomarc233@gmail.com>
2025-10-28 15:46:37 -0500

    Added welcome function

--- collaboration.py
+++ collaboration.py
@@ -6,6 +6,12 @@
 def greeting(name):
     print(f'Hello {name}')
 
+def welcome():
+    print("Welcome to Lavendar Collaboration")
+
 
 if __name__ == "__main__":
     greeting('Gabriel')    
+    welcome()
+
+
